1df55d3e1e72d8bedd819674e55e05a24afc11b4
YURYVOM <112267599+YURYVOM@users.noreply.github.com>
2025-07-02 08:22:47 -0500

    VO

--- src/MEX/01_procesamiento_variables_MEX.R
+++ src/MEX/01_procesamiento_variables_MEX.R
@@ -29,9 +29,9 @@ unique(base_mujeres2$niv)
 
 base_mujeres2 <- base_mujeres2 %>%
   mutate(
-    nivel_educ_agrupado = case_when(
-      edad < 15 | niv == -1 ~ "98",                                # No aplica
-      niv == 99 | (edad >= 15 & is.na(niv)) ~ "99",                # No especificado / NSNR
+    anoest  = case_when(
+      edad_muj < 15 | niv == -1 ~ "98",                                # No aplica
+      niv == 99 | (edad_muj >= 15 & is.na(niv)) ~ "99",                # No especificado / NSNR
       niv == 0 ~ "1",                                              # Ninguno
       niv %in% 1:2 ~ "2",                                          # Preescolar o primaria
       niv %in% 3:7 ~ "3",                                          # Secundaria, técnica con secundaria o media
@@ -40,22 +40,29 @@ base_mujeres2 <- base_mujeres2 %>%
     ),
     
     edad = case_when(
-      edad < 15 ~ "1",
-      edad < 30 ~ "2",
-      edad < 45 ~ "3",
-      edad < 65 ~ "4",
-      TRUE ~ "5"),
+      edad_muj < 15 ~ "1",
+      edad_muj < 30 ~ "2",
+      edad_muj < 45 ~ "3",
+      edad_muj < 65 ~ "4",
+      TRUE~"5"),
     
     dam = ent,
     
     area = t_loc_ur,
     
     etnia = case_when(
-      P3_7 == 1 ~ "1",      #Afro
-      P3_12 == 1 ~ "2",     #indigena
+      p3_7 == 1 ~ "1",      #Afro
+      p3_12 == 1 ~ "2",     #indigena
       TRUE ~ "3"            #Otros
     )
   )
     
     
-  
+################################################################################
+###---------------------------- Indicators ----------------------------------###
+################################################################################  
+
+### Proporción de mujeres y niñas de 15 años o más que hayan tenido pareja alguna 
+### vez y hayan sufrido violencia física, sexual o psicológica por parte de una pareja 
+## actual o anterior en los últimos 12 meses (15-49 años)
+
